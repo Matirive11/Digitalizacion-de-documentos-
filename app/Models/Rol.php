0d8af9c0f0a5c2bf63a0b16f1d808a7dd0b8a63f
Pabl0P11 <pablo.ponce@educacionadventista.org.ar>
2024-10-02 21:02:00 +0000

    Merge

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -13,3 +13,19 @@ class Rol extends Model
 
     protected $table = 'rol';
 }
+
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rol extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+    public $timestamps = false; // Desactiva los timestamps
+
+    protected $table = 'rol';
+}
