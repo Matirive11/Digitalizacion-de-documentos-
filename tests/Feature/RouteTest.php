22d7c3c445741ba31f8b290817f4a808ba6e74ab
Pabl0P11 <pablo.ponce@educacionadventista.org.ar>
2025-03-01 18:51:12 -0300

    Cambios

--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -1,17 +1,8 @@
 <?php
 
-use function Pest\Laravel\get;
-
 // 🟢 Prueba para rutas públicas
-test('las rutas públicas son accesibles', function ($url) {
-    get($url)->assertOk(); // 🔹 Corrección aquí
-})->with([
-    '/',
-]);
+test('las rutas públicas son accesibles', closure: function () {
+    $this->get('/')->assertOk(); // 🔹 Corrección aquí
+});
 
-// 🔴 Prueba para rutas privadas
-test('las rutas privadas requieren autenticación', function ($url) {
-    get($url)->assertRedirect('/login'); // 🔹 Corrección aquí
-})->with([
-    '/dashboard',
-]);
+
